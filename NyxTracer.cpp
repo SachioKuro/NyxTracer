2ee7f6e9ac6754fb4d000f455bff301d427eeb83
Sascha Bussian <sachio.kuro88@gmail.com>
2023-01-29 20:26:07 +0100

    change to CMAKE

---
+++ NyxTracer.cpp
@@ -0,0 +1,12 @@
+﻿// NyxTracer.cpp : Defines the entry point for the application.
+//
+
+#include "NyxTracer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
